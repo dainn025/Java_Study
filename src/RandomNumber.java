fc196c6ff823646ae498907671087622ae1b0152
dain <dainn025@gmail.com>
2026-04-01 23:09:35 +0900

    RandomNumber Class 작성

---
+++ src/RandomNumber.java
@@ -0,0 +1,39 @@
+import java.util.Random;
+
+public class RandomNumber {
+    private int[] numbers; // 랜덤숫자를 저장하는 변수
+
+    // 생성자 (배열 3칸만 생성되도록 초기화)
+    RandomNumber() {
+        this.numbers = new int[3];
+    }
+
+    // 랜덤숫자 생성하는 메소드
+    public void generate() {
+        Random random = new Random();
+        boolean b_overlap = false;
+
+        for (int i = 0; i < numbers.length; i++) {
+            int num = 0;
+
+            do {
+                num = random.nextInt(9) + 1;
+
+                for (int j = 0; j < i; j++) {
+                    if (numbers[j] == num) {
+
+                        // 숫자 중복 O
+                        b_overlap = true;
+                        break;
+                    }
+                }
+            } while (b_overlap);
+            numbers[i] = num;
+        }
+    }
+
+    // 랜덤 숫자를 return 하는 메소드
+    public int[] getNumbers() {
+        return this.numbers;
+    }
+}
